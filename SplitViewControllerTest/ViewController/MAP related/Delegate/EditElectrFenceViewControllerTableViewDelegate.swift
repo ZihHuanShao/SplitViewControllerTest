c5867def92463ed78241af1f50375b58d8827946
fred <fred@maxkit.com.tw>
2020-04-27 15:59:26 +0800

    Code tuning

--- SplitViewControllerTest/ViewController/MAP related/Delegate/EditElectrFenceViewControllerTableViewDelegate.swift
+++ SplitViewControllerTest/ViewController/MAP related/Delegate/EditElectrFenceViewControllerTableViewDelegate.swift
@@ -95,13 +95,19 @@ extension EditElectrFenceViewControllerTableViewDelegate: UITableViewDataSource 
 //        return ""
 //    }
     
-    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return ""
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return nil
+        } else {
+            return "123"
+        }
     }
+    
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return CGFloat(0.1)
     }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "testcell", for: indexPath)
