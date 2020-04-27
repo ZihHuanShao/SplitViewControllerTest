f524a3c9274662a829e46f5c7c0da5cd688ea16d
fred <fred@maxkit.com.tw>
2020-04-27 18:04:17 +0800

    UI tuning

---
+++ SplitViewControllerTest/Cell/Map related/EditElectrFenceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EditElectrFenceTableViewCell.swift
+//  SplitViewControllerTest
+//
+//  Created by kokome maxkit on 2020/4/27.
+//  Copyright © 2020 fredshao. All rights reserved.
+//
+
+import UIKit
+
+class EditElectrFenceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
